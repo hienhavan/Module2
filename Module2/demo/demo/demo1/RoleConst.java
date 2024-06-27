c9e8fb38e6376d2ba6e8e8ac1ba818692533bc8f
hientb <havanhien54321@gmail.com>
2024-06-27 17:01:21 +0700

    bai16

--- Module2/demo/demo/demo1/RoleConst.java
+++
@@ -1,6 +0,0 @@
-package demo.demo.demo1;
-
-public interface RoleConst {
-    int ROLE_ADMIN = 1;
-    int ROLE_USER = 2;
-}
--- Module2/demo/demo/demo1/User.java
+++ Module2/demo/demo/demo1/User.java
@@ -2,16 +2,17 @@ package demo.demo.demo1;
 
 public class User {
     private String name;
-    private String email;
-    private int role;
+    private String phone;
+    private String gender;
 
     public User() {
     }
 
-    public User(String name, String email, int role) {
+    public User(String name, String phone, String gender) {
+
         this.name = name;
-        this.email = email;
-        this.role = role;
+        this.phone = phone;
+        this.gender = gender;
     }
 
     public String getName() {
@@ -22,28 +23,24 @@ public class User {
         this.name = name;
     }
 
-    public String getEmail() {
-        return email;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
-    public int getRole() {
-        return role;
+    public String getGender() {
+        return gender;
     }
 
-    public void setRole(int role) {
-        this.role = role;
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     @Override
     public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", role=" + role +
-                '}';
+        return name + "," + phone + "," + gender;
     }
 }
