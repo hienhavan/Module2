6b3902bd4e72b07a122754dc05ee21a70862c207
hienhavan <havanhien54321@gmail.com>
2024-07-11 20:20:24 +0700

    bai thuc hanh

--- Module2/demo/demobaithithuchanh/MedicalRecordFactory.java
+++
@@ -1,6 +0,0 @@
-package demo.demobaithithuchanh;
-
-public interface MedicalRecordFactory {
-    MedicalRecord createMedicalRecord(int id, String code, String namePatient, String hospitalizedDay, String hospitalDischargeDate, String reasonForHospitalization);
-}
-
